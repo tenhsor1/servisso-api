094dc916e4312e10d8254e4f40c876e812803c7a
radamakoa <radames.ramirez.perez@hotmail.com>
2016-04-16 01:04:09 -0500

    add validation for exist folder in utils method

--- app/Extensions/utils.php
+++ app/Extensions/utils.php
@@ -9,13 +9,22 @@ use App\Http\Requests;
 */
 class Utils extends Model{
 	 public static function StorageImage($id, $request, $path='\\public\image\\', $path_thumb='\\public\thumb\\'){   
+		$carpeta = '../../servisso-api/public/image';
+		if (!file_exists($carpeta)) {
+		   mkdir($carpeta, 0777, true);
+		} 
+		$carpeta = '../../servisso-api/public/thumb';
+		if (!file_exists($carpeta)) {
+		   mkdir($carpeta, 0777, true);
+		} 
 		//Ruta donde queremos guardar las imagenes
 		$path = base_path().$path;
 		$path_thumb = base_path().$path_thumb;
 		//se obtiene el archivo
 		$file = $request->file('image');
 		//Se obtiene la extension de la imagen
-		$ext = $file->getClientOriginalExtension();
+		//$ext = $file->getClientOriginalExtension();
+		$ext = 'png';
 		//se obtiene la fecha en formato entero
 		$time = strtotime('now');
 		//se crea un nombre para la imagen tamaño normal y la reducida
