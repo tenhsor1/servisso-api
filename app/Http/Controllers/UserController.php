95b653c75578bb1dc61d8d246cb7d9d4c48aca9b
Rick <tenhsor@mac-rick.local>
2016-09-03 17:36:16 -0500

    Changed from 410 to 422 so the form error handler reads it

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -388,7 +388,7 @@ class UserController extends Controller
 			if($userRequested->roleAuth  == "ADMIN")
 				$user = User::find(0);
 
-			if($user->invitations > 0){				
+			if($user->invitations > 0){
 
 				$key = config('app.key');
 				$code = hash_hmac('sha256', str_random(40), $key);
@@ -427,8 +427,8 @@ class UserController extends Controller
 				}
 
 			}else{
-				$errorJSON = ['error'   => 'You do not have more invitations available', 'code' => 410];//Gone
-				return response()->json($errorJSON, 410);
+				$errorJSON = ['error'   => 'You do not have more invitations available', 'code' => 422];
+				return response()->json($errorJSON, 422);
 			}
 		}else{
 			$errorJSON = ['error'   => 'Unauthorized', 'code' => 403];
